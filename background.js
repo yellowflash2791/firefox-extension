dd7fc75008bab476a7afea103dff5f79c3cc8f96
yellowflash2791 <anthonyarnold@Anthonys-MacBook-Pro.local>
2023-09-28 15:55:01 +0800

    add trial code

--- background.js
+++ background.js
@@ -1,45 +1,66 @@
-//importScripts('ExtPay.js');
-
-var extpay = ExtPay('browsesafe'); // Careful! See note below
+var extpay = ExtPay('browsesafe'); // Initialize with your extension's name
 extpay.startBackground();
 
-let isBlocking = false; // Initialize this to false
-let isPaid = false; // Initialize this to false
+let isBlocking = false; // Track blocking state
+let isPaid = false; // Track payment state
+let isTrialActive = false; // Track trial state
+let trialDaysLeft = 7; // Initialize with 7 days for the trial
 
 function checkPaymentStatus() {
-  extpay.getUser().then(user => {
-    if (user.paid) {
-      isPaid = true;
-    } else {
-      isPaid = false;
-    }
-  });
+    extpay.getUser().then(user => {
+        const now = new Date();
+        const sevenDays = 1000 * 60 * 60 * 24 * 7;
+
+        isPaid = user.paid;
+
+        if (user.trialStartedAt && (now - new Date(user.trialStartedAt)) < sevenDays) {
+            isTrialActive = true;
+            trialDaysLeft = 7 - Math.floor((now - new Date(user.trialStartedAt)) / (1000 * 60 * 60 * 24));
+        } else {
+            isTrialActive = false;
+            trialDaysLeft = 0;
+        }
+    });
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "getState") {
-    sendResponse({isBlocking: isBlocking, isPaid: isPaid});
-  } else if (message.action === "toggleState") {
-    isBlocking = !isBlocking; // Toggle based on user input
-    sendResponse({isBlocking: isBlocking});
-  } else if (message.action === "goToPaymentPage") {
-        extpay.openPaymentPage();
-
-  } else if (message.action === "goToLoginPage") {
-        extpay.openLoginPage();  // Open the login page
-  } 
-
-
-
+    switch (message.action) {
+        case "getState":
+            sendResponse({
+                isBlocking: isBlocking,
+                isPaid: isPaid,
+                isTrialActive: isTrialActive,
+                trialDaysLeft: trialDaysLeft
+            });
+            break;
+        case "toggleState":
+            isBlocking = !isBlocking; // Toggle based on user input
+            sendResponse({
+                isBlocking: isBlocking,
+                isPaid: isPaid,
+                isTrialActive: isTrialActive,
+                trialDaysLeft: trialDaysLeft
+            });
+            break;
+        case "goToPaymentPage":
+            extpay.openPaymentPage();
+            break;
+        case "goToLoginPage":
+            extpay.openLoginPage(); // Open the login page
+            break;
+        case "goToTrialPage":
+            extpay.openTrialPage('7-day'); // Open the 7-day trial page
+            break;
+        default:
+            break;
+    }
 });
 
 chrome.tabs.onCreated.addListener((tab) => {
-  if (isBlocking && isPaid) {
-    chrome.tabs.remove(tab.id);
-  }
+    if (isBlocking && (isPaid || isTrialActive)) {
+        chrome.tabs.remove(tab.id);
+    }
 });
-
-
 
 setInterval(checkPaymentStatus, 3000);
 
--- popup.js
+++ popup.js
@@ -1,77 +1,106 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const enableButton = document.getElementById("enableButton");
-  const paymentStatus = document.getElementById("paymentStatus");
-  const payButton = document.getElementById("payButton");
-  const loginButton = document.getElementById("loginButton");
-  const cancelSubscriptionButton = document.getElementById("cancelSubscriptionButton");
-  const cancelSubscribedText = document.getElementById("cancelSubscribedText");
-
-  let isBlocking = false;  // Initialize state
-  let isPaid = false;  // Initialize payment state
-
-  // Fetch initial states from background script
-  chrome.runtime.sendMessage({action: "getState"}, function(response) {
-    isBlocking = response.isBlocking;
-    isPaid = response.isPaid;
-    updateUI();  // Update UI based on fetched states
-  });
+    const enableButton = document.getElementById("enableButton");
+    const paymentStatus = document.getElementById("paymentStatus");
+    const payButton = document.getElementById("payButton");
+    const trialStatus = document.getElementById("trialStatus");
+    const trialDaysLeft = document.getElementById("trialDaysLeft");
+    const loginButton = document.getElementById("loginButton");
+    const startTrialText = document.getElementById("startTrialText");
+    const startTrialButton = document.getElementById("startTrialButton"); 
+    const cancelSubscriptionButton = document.getElementById("cancelSubscriptionButton");
+    const cancelSubscribedText = document.getElementById("cancelSubscribedText");
+    const subscribedText= document.getElementById("subscribedText");
 
 
-  const subscribedText = document.getElementById("subscribedText"); // Get reference to the "Subscribed?" text
+    let isBlocking = false;
 
-
-  function updateUI() {
-    if (isPaid) {
-       paymentStatus.textContent = "Active";
-       payButton.style.display = "none";
-       loginButton.style.display = "none";
-       enableButton.style.display = "block";
-       subscribedText.style.display = "none"; 
-       paymentStatus.classList.remove('inactive-status');
-       paymentStatus.classList.add('active-status');
-       cancelSubscribedText.style.display = "block";
-       cancelSubscriptionButton.style.display = "block";
-       if (isBlocking) {
-         enableButton.textContent = "ON";
-         enableButton.style.backgroundColor = "green";
-       } else {
-         enableButton.textContent = "OFF";
-          enableButton.style.backgroundColor = "red";
-       }
-
-    } else {
-      paymentStatus.textContent = "Inactive";
-      payButton.style.display = "block";
-      loginButton.style.display = "block";
-      enableButton.style.display = "none";
-      subscribedText.style.display = "block"; // Show the "Subscribed?" text if user is not paid
-      cancelSubscribedText.style.display = "none";
-      cancelSubscriptionButton.style.display = "none";
-      paymentStatus.classList.add('inactive-status');
-      paymentStatus.classList.remove('active-status'); 
-    }
-  }
-
-  enableButton.addEventListener("click", function() {
-    isBlocking = !isBlocking; 
-    chrome.runtime.sendMessage({action: "toggleState"}, function(response) {
-      isBlocking = response.isBlocking; 
-      updateUI();
+    // Fetch initial states from background script
+    chrome.runtime.sendMessage({ action: "getState" }, function(response) {
+        isBlocking = response.isBlocking;
+        updateUI(response); // Update UI based on fetched states
     });
-  });
 
-  payButton.addEventListener("click", function() {
-    chrome.runtime.sendMessage({action: "goToPaymentPage"});
-  });
+    function updateUI(data) {
+        const { isPaid, isTrialActive, trialDaysLeft: daysLeft } = data;
 
-  loginButton.addEventListener("click", function() {
-     chrome.runtime.sendMessage({action: "goToLoginPage"});
-  });
+        if (isTrialActive) {
+            trialStatus.textContent = "On Trial";
+            trialStatus.style.display = "block";
+            trialStatus.style.backgroundColor = "yellow";
+            trialDaysLeft.textContent = `${daysLeft} days remaining`;
+            trialDaysLeft.style.display = "block";
+            paymentStatus.style.display = "none";
+            payButton.style.display = "none";
+            enableButton.style.display = "block";
+            startTrialText.style.display = "none";
+            cancelSubscriptionButton.style.display="none";
+            cancelSubscribedText.style.display="none";
+            loginButton.style.display = "none";
+            subscribedText.style.display = "none";
+            startTrialButton.style.display = "none";
 
-  cancelSubscriptionButton.addEventListener("click", function() {
-    chrome.runtime.sendMessage({action: "goToPaymentPage"});
 
-  });
+            if (isBlocking) {
+                enableButton.textContent = "ON";
+                enableButton.style.backgroundColor = "green";
+            } else {
+                enableButton.textContent = "OFF";
+                enableButton.style.backgroundColor = "red";
+            }
+        } else if (isPaid) {
+            paymentStatus.textContent = "Subscription Active";
+            paymentStatus.style.color = "green";
+            paymentStatus.style.display = "block";
+            payButton.style.display = "none";
+            enableButton.style.display = "block";
+            trialStatus.style.display = "none";
+            trialDaysLeft.style.display = "none";
+            startTrialText.style.display = "none";
+            cancelSubscriptionButton.style.display="block";
+            cancelSubscribedText.style.display="block";
+            loginButton.style.display = "none";
+            subscribedText.style.display = "none";
+            startTrialButton.style.display = "none";
 
+            if (isBlocking) {
+                enableButton.textContent = "ON";
+                enableButton.style.backgroundColor = "green";
+            } else {
+                enableButton.textContent = "OFF";
+                enableButton.style.backgroundColor = "red";
+            }
+        } else {
+            paymentStatus.textContent = "Inactive";
+            paymentStatus.style.color = "red";
+            paymentStatus.style.display = "block";
+            payButton.style.display = "block";
+            enableButton.style.display = "none";
+            trialStatus.style.display = "none";
+            trialDaysLeft.style.display = "none";
+            startTrialText.style.display = "block";
+            startTrialButton.style.display = "block";
+        }
+    }
+
+    enableButton.addEventListener("click", function() {
+        isBlocking = !isBlocking;
+        chrome.runtime.sendMessage({ action: "toggleState" }, function(response) {
+            isBlocking = response.isBlocking;
+            updateUI(response);
+        });
+    });
+
+    payButton.addEventListener("click", function() {
+        chrome.runtime.sendMessage({ action: "goToPaymentPage" });
+    });
+
+    loginButton.addEventListener("click", function() {
+        chrome.runtime.sendMessage({ action: "goToLoginPage" });
+    });
+
+    // Event listener for starting a trial
+    startTrialButton.addEventListener("click", function() {
+        chrome.runtime.sendMessage({ action: "goToTrialPage" });
+    });
 });
 
